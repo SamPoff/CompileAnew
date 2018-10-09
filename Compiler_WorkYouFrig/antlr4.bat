f53205f5dd6292c80d215f3b4ea768816c57e3be
Sam Poff <spoff42@gmail.com>
2018-10-09 04:57:24 -0700

    Updated bats and have java parse files.

--- Compiler_WorkYouFrig/antlr4.bat
+++ Compiler_WorkYouFrig/antlr4.bat
@@ -1,3 +1,4 @@
-SET CLASSPATH=.;C:\Users\Sam\git\repository2\Compiler_WorkYouFrig\antlr-4.7.1-complete.jar;%CLASSPATH%
+SET CLASSPATH=.;C:\Users\Sam\git\repository2\Compiler_WorkYouFrig\antlr-4.5.3-complete.jar;%CLASSPATH%
 java org.antlr.v4.Tool %*
+doskey antlr4=java org.antlr.v4.Tool $*
 cmd /k 
