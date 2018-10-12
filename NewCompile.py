ce835ec9f6b599cf0a3d3720e23b6ceee83e6398
HarrisonFitch <harrisonfitch13@gmail.com>
2018-10-11 20:08:35 -0700

    Rules updated
    Unary addition and subtraction
    while loops

---
+++ NewCompile.py
@@ -0,0 +1,39 @@
+'''
+Created on Oct 5, 2018
+
+@author: Fitch
+@author: Sam
+'''
+
+class NewCompile(object):
+    
+        def __init__(self, filename):  
+            self.filename = filename
+            self.rules = rules = [
+    ('(\/\*[\w\'\s\r\n\*]*\*\/)|(\/\/[\w\s\']*)|(\<![\-\-\s\w\>\/]*\>)',    'COMMENT', None),
+    ('\d+',                         'NUMBER', 60),
+    ('int',                         'TYPE_INT', 60),
+    ('char',                        'TYPE_CHAR', 5),
+    ('main',                        'MAIN', None),
+    ('return',                      'RETURN', 5),
+    ('else',                        'ELSE', 10),
+    ('if',                          'IF', 10),
+    ('for',                         'FOR', 10),
+    ('while',                       'WHILE', 10),
+    ('[_a-zA-Z][_a-zA-Z0-9]{0,31}', 'IDENTIFIER', 60),
+    ('\{',                          'LB', 60),
+    ('\}',                          'RB', 60),
+    ('(\-)(\-)',                    'DOUBLE_MINUS', 60),
+    ('(\+)(\+)',                    'DOUBLE_PLUS', 60),
+    ('\;',                          'SEMICOLON', 70),
+    ('\<=',                         'LESSTHANEQUAL', 20),
+    ('\<',                          'LESSTHAN', 20),
+    ('\>=',                         'GREATERTHANEQUAL', 20),
+    ('\>',                          'GREATERTHAN', 20),
+    ('\+',                          'PLUS', 10),
+    ('\-',                          'MINUS', 10),
+    ('\(',                          'LP', 40),
+    ('\)',                          'RP', 40),
+    ('==',                          'EQUALVALUE', 20),
+    ('=',                           'EQUALSIGN', 30),
+    ]
