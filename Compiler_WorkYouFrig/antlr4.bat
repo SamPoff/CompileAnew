934e78edcf8d9853765245c19e609e984208ab27
Samuel Poff <samuel.poff@aero.org>
2018-11-29 06:53:17 -0800

    Removed some stuff.

--- Compiler_WorkYouFrig/antlr4.bat
+++
@@ -1,4 +0,0 @@
-SET CLASSPATH=.;C:\Users\Sam\git\repository2\Compiler_WorkYouFrig\antlr-4.5.3-complete.jar;%CLASSPATH%
-java org.antlr.v4.Tool %*
-doskey antlr4=java org.antlr.v4.Tool $*
-cmd /k 
--- Compiler_WorkYouFrig/grun.bat
+++
@@ -1,4 +0,0 @@
-SET CLASSPATH=.;C:\Users\Sam\git\repository2\Compiler_WorkYouFrig\antlr-4.5.3-complete.jar;%CLASSPATH%
-java org.antlr.v4.gui.TestRig %*
-doskey grun =java org.antlr.v4.gui.TestRig $*
-cmd /k 
